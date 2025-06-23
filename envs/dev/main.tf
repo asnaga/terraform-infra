f8129c8b80cb5fc1badbd348f7c61d8e65ede6ce
Ashish NG <94837964+asnaga@users.noreply.github.com>
2025-06-23 14:17:26 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,39 +1,39 @@
 provider "aws" { region = var.region }
 
 module "vpc" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//vpc?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//modules/vpc?ref=main"
   vpc_cidr    = var.vpc_cidr
   environment = var.environment
 }
 
 module "elb" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//elb?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//modules/elb?ref=main"
   vpc_id      = module.vpc.vpc_id
   subnets     = module.vpc.public_subnets
   environment = var.environment
 }
 
 module "ecs" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//ecs?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//modules/ecs?ref=main"
   subnets     = module.vpc.public_subnets
   environment = var.environment
 }
 
 module "rds" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//rds?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//modules/rds?ref=main"
   vpc_id      = module.vpc.vpc_id
   subnets     = module.vpc.public_subnets
   environment = var.environment
 }
 
 module "redis" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//redis?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//modules/redis?ref=main"
   vpc_id      = module.vpc.vpc_id
   subnets     = module.vpc.public_subnets
   environment = var.environment
 }
 
 module "s3" {
-  source      = "git::https://github.com/asnaga/terraform-modules.git//S3?ref=main"
+  source      = "git::https://github.com/asnaga/terraform-modules.git//s3/modules?ref=main"
   environment = var.environment
 }
