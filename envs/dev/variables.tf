ff281543718d0f19a2c578e1a532490ddaf17d5d
Ashish NG <94837964+asnaga@users.noreply.github.com>
2025-06-23 14:24:37 +0530

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,3 +1,13 @@
-variable "region" { type = string; default = "eu-west-1" }
-variable "vpc_cidr" { type = string; default = "172.16.0.0/20" }
-variable "environment" { type = string }
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "172.16.0.0/20"
+}
+
+variable "environment" {
+  type = string
+}
